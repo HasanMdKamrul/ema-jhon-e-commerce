e5b38eeed68fb29c734cf82aaa9c24e333921108
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-24 23:42:33 +0200

    Styling fixes added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './Product.css';
 
-const Product = ({product:{name,id,img,price,category,ratingsCount}}) => {
+const Product = ({product,product:{name,id,seller,img,price,category,ratings}}) => {
+    console.log(product)
     return (
         <div className='card'>
-            <img src={img} alt="" />
+            <img src={img?img:"N/A"} alt="" />
             <div className='product-content'>
                 <p className='product-name'>{name}</p>
                 <p>Price:${price}</p>
+                <div className='product-extranal-info'>
+                    <p>Manufacturer:{seller}</p>
+                    <p>Ratings:{ratings}stars</p>
+                </div>
             </div>
+            <button className='cart-button'>Add To Cart</button>
         </div>
     );
 };
