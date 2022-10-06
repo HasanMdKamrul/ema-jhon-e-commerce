c551b8992d6385eb509d3b1d54dfb3ff8664ba7d
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-07 01:31:17 +0200

    Design fixes for the cart container

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,25 +1,27 @@
-import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Cart from '../Cart/Cart';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Orders = () => {
-    const {cart : cartData,products} = useLoaderData();
-   
+  const { cart: cartData, products } = useLoaderData();
 
-    // ** cart data paisi but amra user k ekta delete button dibo tai sorasori cart data ta use korbo na ekta state a rekhe use korbo bcz user delete korle amader cart er data  Change hote pare
+  // ** cart data paisi but amra user k ekta delete button dibo tai sorasori cart data ta use korbo na ekta state a rekhe use korbo bcz user delete korle amader cart er data  Change hote pare
 
-    const [cart,setCart] = useState(cartData);
+  const [cart, setCart] = useState(cartData);
 
-    return (
-        <div className='shop-container'>
-          <div className='products-container'>
-
-          </div>
-          <div className='cart-container'>
-            <Cart cart={cart} />
-          </div>
-        </div>
-    );
+  return (
+    <div className="shop-container">
+      <div className="orders-container">
+        {
+            cart.map(product => <ReviewItem product={product} key={product.id}/>)
+        }
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart} />
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,32 @@
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "./ReviewItem.css";
+
+const ReviewItem = ({ product: { price, name, img, quantity, shipping } }) => {
+  return (
+    <div className="order-items">
+      <div className="order-img-container">
+        <img src={img} alt="" />
+      </div>
+      <div className="order-content">
+        <div className="order-content-text">
+          <p>{name}</p>
+          <p>
+            <small>Price:{price}</small>
+          </p>
+          <p>
+            <small>Quantity:{quantity}</small>
+          </p>
+        </div>
+        <div>
+          <button  className="icon-container">
+            <FontAwesomeIcon className="icon-btn" icon={faTrashAlt}/>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewItem;
